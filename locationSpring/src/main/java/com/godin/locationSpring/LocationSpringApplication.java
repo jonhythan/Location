c05e0e976a94604ee44b70670c209fdb71148be6
Jonathan <jonathanargueta@outlook.fr>
2022-12-05 17:39:09 -0500

    Ajout du projet Spring

---
+++ locationSpring/src/main/java/com/godin/locationSpring/LocationSpringApplication.java
@@ -0,0 +1,13 @@
+package com.godin.locationSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocationSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocationSpringApplication.class, args);
+	}
+
+}
---
+++ locationSpring/src/test/java/com/godin/locationSpring/LocationSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.godin.locationSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocationSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
