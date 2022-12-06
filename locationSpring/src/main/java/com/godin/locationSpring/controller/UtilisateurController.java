70add32b78187ab380ca56aef4e414feb2b2dd34
Jonathan <jonathanargueta@outlook.fr>
2022-12-06 18:46:04 -0500

    ajout d'un utilisateur à partir de la page Login

--- locationSpring/src/main/java/com/godin/locationSpring/controller/UtilisateurController.java
+++ locationSpring/src/main/java/com/godin/locationSpring/controller/UtilisateurController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,6 +15,7 @@ import com.godin.locationSpring.model.Utilisateur;
 import com.godin.locationSpring.repository.UtilisateurRepository;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class UtilisateurController {
 	@Autowired
 	UtilisateurRepository utilisateurRepository;
--- location_react_app/src/Components/Login.js
+++ location_react_app/src/Components/Login.js
@@ -4,7 +4,7 @@ const Login = ()=>{
     const [prenom, setPrenom]=useState("");
     const [nom, setNom]=useState("");
     const [telephone, setTelephone]=useState("");
-    const [email, setEmail]=useState("");
+    const [courriel, setCourriel]=useState("");
     const [mdp, setMdp]=useState("");
     const [cmdp, setCmdp]=useState("");
     const [numRue, setNumRue]=useState("");
@@ -20,33 +20,23 @@ const Login = ()=>{
         const requestOptions={
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
-            body: { 
-                "nom": "jonathan",
-                "prenom" : "jonathan",
-                "courriel": "jon@jona2",
-                "telephone": "5147972874",
-                "password":"abc",
-                "numRue": "655",
-                "nomRue": "Chritophe-Colomb",
-                "ville":"Montréal",
-                "province": "Québec",
-                "codePostal":"h2s-2h1"
-                // "prenom": prenom,
-                // "nom":nom,
-                // "telephone":telephone,
-                // "email":email,
-                // "mdp":mdp,
-                // "cmdp":cmdp,
-                // "numRue":numRue,
-                // "nomRue":nomRue,
-                // "ville":ville,
-                // "province":province,
-                // "codePostal":codePostal                
-            }
+            body: JSON.stringify({ 
+                "prenom": prenom,
+                "nom":nom,
+                "telephone":telephone,
+                "courriel":courriel,
+                "password":mdp,
+                "numRue":numRue,
+                "nomRue":nomRue,
+                "ville":ville,
+                "province":province,
+                "codePostal":codePostal                
+            })
         }
 
         fetch('http://localhost:8080/utilisateur', requestOptions)
-        .then(response => console.log(response));
+        .then(response => response.text())
+        .then(data=>console.log(data));
 
     }
 
@@ -54,23 +44,23 @@ const Login = ()=>{
         <>
         <h2>Afin d'enregistrer votre compte, veuillez entrer des informations valides</h2>
         <form onSubmit={handleSubmit}>
-            {/* <input type="text" value={prenom} onChange={e=>setPrenom(e.target.value)} placeholder="Prénom" required/>
+            <input type="text" value={prenom} onChange={e=>setPrenom(e.target.value)} placeholder="Prénom" required/>
             <input type="text" value={nom} onChange={e=>setNom(e.target.value)} placeholder="Nom" required/>
             <input type="text" value={telephone} onChange={e=>setTelephone(e.target.value)} placeholder="Téléphone" pattern="[1-9]{10}" required/>
-            <input type="email" value={email} onChange={e=>setEmail(e.target.value)} placeholder="Émail" required/>
+            <input type="email" value={courriel} onChange={e=>setCourriel(e.target.value)} placeholder="Émail" required/>
             <input type="text" value={mdp} onChange={e=>setMdp(e.target.value)} placeholder="Mot de passe" required/>
             <input type="text" value={cmdp} onChange={e=>setCmdp(e.target.value)} placeholder="Confirmation mot de passe" required/>
 
             <br/>
             <span>Détails de l'adresse</span>
             <br></br>
-            <input type="text" value={numRue} onChange={e=>setNumRue(e.target.value)} placeholder="Numéro rue"/>
+            <input type="text" value={numRue} onChange={e=>setNumRue(e.target.value)} pattern="[1-9]" placeholder="Numéro rue"/>
             <input type="text" value={nomRue} onChange={e=>setNomRue(e.target.value)} placeholder="Nom rue"/>
             <input type="text" value={ville} onChange={e=>setVille(e.target.value)} placeholder="Ville"/>
             <input type="text" value={province} onChange={e=>setProvince(e.target.value)} placeholder="Province"/>
-            <input type="text" value={codePostal} onChange={e=>setCodePostal(e.target.value)} placeholder="Code Postal" pattern="[a-zA-z]{3}-[a-zA-z]{3}" required/>
+            <input type="text" value={codePostal} onChange={e=>setCodePostal(e.target.value)} placeholder="Code Postal" pattern="[a-zA-z1-9]{3}-[a-zA-z1-9]{3}" required/>
 
-             */}
+            
             <input type="submit" value="Continuer"/>
         </form>
         </>
