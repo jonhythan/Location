fc18cb5bed97784558415d59a6d52092802b11ec
Jonathan <jonathanargueta@outlook.fr>
2022-12-07 00:02:45 -0500

    Ajout style au Login

--- location_react_app/src/Components/Login.js
+++ location_react_app/src/Components/Login.js
@@ -1,4 +1,5 @@
 import {useState} from 'react'
+import logo from '../Logo.svg'
 
 const Login = ()=>{
     const [prenom, setPrenom]=useState("");
@@ -41,29 +42,51 @@ const Login = ()=>{
     }
 
     return(
-        <>
-        <h2>Afin d'enregistrer votre compte, veuillez entrer des informations valides</h2>
-        <form onSubmit={handleSubmit}>
-            <input type="text" value={prenom} onChange={e=>setPrenom(e.target.value)} placeholder="Prénom" required/>
-            <input type="text" value={nom} onChange={e=>setNom(e.target.value)} placeholder="Nom" required/>
-            <input type="text" value={telephone} onChange={e=>setTelephone(e.target.value)} placeholder="Téléphone" pattern="[1-9]{10}" required/>
-            <input type="email" value={courriel} onChange={e=>setCourriel(e.target.value)} placeholder="Émail" required/>
-            <input type="text" value={mdp} onChange={e=>setMdp(e.target.value)} placeholder="Mot de passe" required/>
-            <input type="text" value={cmdp} onChange={e=>setCmdp(e.target.value)} placeholder="Confirmation mot de passe" required/>
+        <div > 
+            <img src={logo} alt="Logo"/>
+            <h2 className="text-center text-white">Créez votre compte</h2>
+            <h4 className="text-center text-secondary">Afin d'enregistrer votre compte, veuillez entrer des informations valides</h4>
+            <form onSubmit={handleSubmit} className="container container-login">
+                <div className="row px-5 p-3">
+                    <input type="text" value={prenom} onChange={e=>setPrenom(e.target.value)} placeholder="Prénom" required className='col '/>
+                    <input type="text" value={nom} onChange={e=>setNom(e.target.value)} placeholder="Nom" required className='col'/>
+                </div>
+                <div className="row px-5 p-3">
+                    <input type="text" value={telephone} onChange={e=>setTelephone(e.target.value)} placeholder="Téléphone" pattern="[1-9]{10}" required className='col'/>
+                    <span className='col'></span>
+                </div>
+                <div className="row px-5 p-3">
+                    <input type="email" value={courriel} onChange={e=>setCourriel(e.target.value)} placeholder="Émail" required className='col'/>
+                    <span className='col'></span>
+                </div>
+                <div className="row px-5 p-3">
+                    <input type="text" value={mdp} onChange={e=>setMdp(e.target.value)} placeholder="Mot de passe" required  className='col'/>
+                    <input type="text" value={cmdp} onChange={e=>setCmdp(e.target.value)} placeholder="Confirmation mot de passe" required className='col'/>
+                </div>
 
-            <br/>
-            <span>Détails de l'adresse</span>
-            <br></br>
-            <input type="text" value={numRue} onChange={e=>setNumRue(e.target.value)} pattern="[1-9]" placeholder="Numéro rue"/>
-            <input type="text" value={nomRue} onChange={e=>setNomRue(e.target.value)} placeholder="Nom rue"/>
-            <input type="text" value={ville} onChange={e=>setVille(e.target.value)} placeholder="Ville"/>
-            <input type="text" value={province} onChange={e=>setProvince(e.target.value)} placeholder="Province"/>
-            <input type="text" value={codePostal} onChange={e=>setCodePostal(e.target.value)} placeholder="Code Postal" pattern="[a-zA-z1-9]{3}-[a-zA-z1-9]{3}" required/>
+                <br/>
+                <h4 className="text-secondary">Détails de l'adresse</h4>
+                
+                <div className="row px-5 p-3">
+                    <input type="text" value={numRue} onChange={e=>setNumRue(e.target.value)} pattern="[1-9]" placeholder="No" className='col-1'/>
+                    <input type="text" value={nomRue} onChange={e=>setNomRue(e.target.value)} placeholder="Nom rue" className='col-6'/>
+                    <input type="text" value={ville} onChange={e=>setVille(e.target.value)} placeholder="Ville" className='col'/>
 
-            
-            <input type="submit" value="Continuer"/>
-        </form>
-        </>
+                </div>
+
+                <div className="row px-5 p-3">
+                    <input type="text" value={province} onChange={e=>setProvince(e.target.value)} placeholder="Province" className='col'/>
+                    <input type="text" value={codePostal} onChange={e=>setCodePostal(e.target.value)} placeholder="Code Postal" pattern="[a-zA-z1-9]{3}-[a-zA-z1-9]{3}" required className='col-2'/>
+                    <span className='col-3'></span>
+
+                </div>
+                <div className="row px-5 p-3 justify-content-center">
+                    <input type="submit" value="Continuer" className='col-2 btn btn-primary ' />
+                </div>
+                
+            </form>
+
+        </div>
     )
 }
 
