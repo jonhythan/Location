a66b691e4ff9bf0aecf88a83764d6205b1e55b58
Azemat Imani <azemat.imani@gmail.com>
2022-12-19 22:39:05 -0500

    LD-11-12 - En tant que membre, je veux désactiver ou activer une annonce

--- locationSpring/src/main/java/com/godin/locationSpring/controller/AnnonceController.java
+++ locationSpring/src/main/java/com/godin/locationSpring/controller/AnnonceController.java
@@ -4,15 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import javax.print.attribute.standard.Media;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
 import com.godin.locationSpring.model.Annonce;
 import com.godin.locationSpring.service.AnnonceService;
@@ -65,5 +62,15 @@ public class AnnonceController {
 	@GetMapping("annonce/getByUser/{id}")
 	public List<Annonce> getBasicAnnoncesByUserId(@PathVariable String id){
 		return annonceService.getBasicAnnoncesByUserId(Integer.valueOf(id));
+	}
+
+	@PutMapping(value = "annonce/{id}/disable", produces = MediaType.APPLICATION_JSON_VALUE)
+	public Annonce desactiverUnAnnonce( @PathVariable int id){
+		return annonceService.desactiverUnAnnounce(id);
+	}
+
+	@PutMapping(value = "annonce/{id}/enable", produces = MediaType.APPLICATION_JSON_VALUE)
+	public Annonce activerUnAnnonce( @PathVariable int id){
+		return annonceService.activerUnAnnounce(id);
 	}
 }
